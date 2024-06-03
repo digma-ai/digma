5f0fa0ec5b004e316ad1a5812302879c5e42c89b
Shay Keren <shay.keren@gmail.com>
2024-06-03 13:50:47 +0300

    handle file renaming

--- dev/troubleshooting/plugin-log-watcher/plugin-logfile-monitor.py
+++ dev/troubleshooting/plugin-log-watcher/plugin-logfile-monitor.py
@@ -7,6 +7,11 @@ from typing import Dict
 
 search_terms = ["UI was frozen for", "Digma: Api call", "Digma: Exception"]
 
+
+def get_inode(filename):
+    stat_info = os.stat(filename)
+    return stat_info.st_ino
+
 def get_selected_idea_path():
     idepath = ''
     additional_folder =''
@@ -37,24 +42,33 @@ def get_selected_idea_path():
     return idea_path
 def monitor_log(logfile):
     print(f'start monitoring: {logfile}')
-    with open(logfile, "r") as file:
-        # Move the file pointer to the end of the file
-        file.seek(0, 2)
-        
-        while True:
-            line = file.readline()
-            if line:
-                for term in search_terms:
-                    if term in line:
-                        print(line.strip())
+    while True:
+        with open(logfile, "r") as file:
+            # Move the file pointer to the end of the file
+            file.seek(0, 2)
+            previous_inode = None
+            while True:
+                try:
+                    current_inode = get_inode(logfile)
+                    if previous_inode is not None and previous_inode != current_inode:
+                        print(f"File '{logfile}' was moved and replaced, restart the monitor...")
                         break
-            else:
-                # Wait before checking again
-                time.sleep(0.1)
+                    previous_inode = current_inode
+                    line = file.readline()
+                    if line:
+                        for term in search_terms:
+                            if term in line:
+                                print(line.strip())
+                                break
+                    else:
+                        # Wait before checking again
+                        time.sleep(0.1)
+                except FileNotFoundError:
+                    time.sleep(0.1)
+
 
 def run():
     idea_path = get_selected_idea_path()
-    log_path = os.path.join(idea_path, 'idea.log')
-    monitor_log(log_path)
+    monitor_log(os.path.join(idea_path, 'idea.log'))
 if __name__ == "__main__":
     run()
