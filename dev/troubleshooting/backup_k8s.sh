265a6485ba94c81ed01ee99d16397852654fa87c
Shay Keren <shay.keren@gmail.com>
2024-06-18 22:57:03 +0200

    modify db backup

--- dev/troubleshooting/backup_k8s.sh
+++ dev/troubleshooting/backup_k8s.sh
@@ -9,4 +9,6 @@ mkdir -p $folder/postgres
 echo "postgres backup to $folder/postgres"
 
 postgres_pod=$(kubectl get pods -n $namespace -l app=postgres -o jsonpath='{.items[0].metadata.name}')
-kubectl cp -n $namespace $postgres_pod:/var/lib/postgresql/data $folder/postgres
+kubectl exec -n $namespace -it $postgres_pod -- tar --use-compress-program=zstd -cvf /backup.tar.zst -C /var/lib/postgresql/data .
+
+kubectl cp -n $namespace $postgres_pod:/backup.tar.zst $folder/postgres/backup.tar.zst
