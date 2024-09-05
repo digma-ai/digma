0569b32aeef4dec55fb433aca616821b76e039ab
Shay Keren <shay.keren@gmail.com>
2024-09-05 14:07:24 +0300

    Update s3_backup_postgres_k8s.sh

--- dev/troubleshooting/s3_backup_postgres_k8s.sh
+++ dev/troubleshooting/s3_backup_postgres_k8s.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 namespace="$1"
 presigned_url="$2"
-curr_datetime=$(date +'%Y-%m-%d %H:%M:%S')
 
-
-echo "${curr_datetime} starting postgres backup to s3, connecting to pod..."
+echo $(date +'%H:%M:%S') starting postgres backup to s3
 postgres_pod=$(kubectl get pods -n $namespace -l app=postgres -o jsonpath='{.items[0].metadata.name}')
 kubectl exec -n $namespace $postgres_pod -- /bin/sh -c "
-echo 'backup started'
-pg_dump -U postgres -d digma_analytics -F c -f /postgres-data.dump
-apt upadte > /dev/null 2>&1
-apt install -y curl > /dev/null 2>&1
-cd /
-echo 'uploading file..'
-curl -s '${presigned_url}' | bash -s 'postgres-data.dump'
-echo Done
+echo \$(date +'%H:%M:%S') backup started;
+cd /;
+backup_file='postgres-data.dump';
+pg_dump -U postgres -d digma_analytics -F c -f \$backup_file;
+apt upadte > /dev/null 2>&1;
+apt install -y curl > /dev/null 2>&1;
+file_size_bytes=\$(stat -c %s \$backup_file);  # Correct stat usage
+# Calculate file size in MB using awk
+file_size_mb=\$(echo \$file_size_bytes | awk '{printf \"%.2f\", \$1 / (1024 * 1024)}');
+echo File size: \$file_size_mb MB;
+echo \$(date +'%H:%M:%S') uploading file..;
+curl -s '$presigned_url' | bash -s \$backup_file;
 "
