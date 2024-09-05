74f5c0fb4d09c586693fbb0b517ae70ca7a98172
Shay Keren <shay.keren@gmail.com>
2024-09-05 12:40:41 +0300

    Update s3_backup_postgres_k8s.sh

--- dev/troubleshooting/s3_backup_postgres_k8s.sh
+++ dev/troubleshooting/s3_backup_postgres_k8s.sh
@@ -7,6 +7,5 @@ curr_datetime=$(date +'%Y-%m-%d %H:%M:%S')
 echo "${curr_datetime} starting postgres backup to s3"
 postgres_pod=$(kubectl get pods -n $namespace -l app=postgres -o jsonpath='{.items[0].metadata.name}')
     kubectl exec -n $namespace -it $postgres_pod -- sh -c "
-apt upadte > /dev/null 2>&1;apt install -y curl > /dev/null 2>&1;cd /;curl -s ${presigned_url}  | bash -s 'postgres-data.dump';echo Done
+pg_dump -U postgres -d digma_analytics -F c -f /postgres-data.dump;apt upadte > /dev/null 2>&1;apt install -y curl > /dev/null 2>&1;cd /;curl -s '${presigned_url}'  | bash -s 'postgres-data.dump';echo Done
 "
-
